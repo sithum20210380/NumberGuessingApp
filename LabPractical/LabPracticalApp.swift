7277b10a94d67f2d91c69e16c0d5eeddea5ab277
sithum20210380 <85311920+sithum20210380@users.noreply.github.com>
2024-10-09 17:00:03 +0530

    Initial Commit

---
+++ LabPractical/LabPracticalApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabPracticalApp.swift
+//  LabPractical
+//
+//  Created by Sithum Raveesha on 2024-10-09.
+//
+
+import SwiftUI
+
+@main
+struct LabPracticalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LabPracticalTests/LabPracticalTests.swift
@@ -0,0 +1,17 @@
+//
+//  LabPracticalTests.swift
+//  LabPracticalTests
+//
+//  Created by Sithum Raveesha on 2024-10-09.
+//
+
+import Testing
+@testable import LabPractical
+
+struct LabPracticalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
